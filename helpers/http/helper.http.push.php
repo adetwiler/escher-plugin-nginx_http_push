c53a60257833b30e3e604f844631e4a7cf23f455
Andrew Detwiler <adetwiler@adidamnetworks.com>
2013-02-06 04:09:07 +0000

    Adding curl_multi Support

--- helpers/http/helper.http.push.php
+++ helpers/http/helper.http.push.php
@@ -18,14 +18,9 @@ class Plugin_nginx_http_push_Helper_http_push extends Helper {
 
 	function publish($id, $data='1') {
 		$CFG = Load::CFG();
-        if (is_array($CFG['comet']['publish_url'])) {
-            foreach ($CFG['comet']['publish_url'] as $url) {
-                $this->http($url.'?'.$CFG['comet']['channel_name'].'='.$id,'POST',$data);
-            }
-        } else {
-		    $this->http($CFG['comet']['publish_url'].'?'.$CFG['comet']['channel_name'].'='.$id,'POST',$data);
-        }
-		if ($this->http_code != '202') { return false; }
+        $pub_url = $CFG['comet']['publish_url'];
+        if (!is_array($pub_url)) { $pub_url = (array)$pub_url; }
+        $this->http($pub_url,'POST',$CFG['comet']['channel_name'].'='.$id,$data);
 		return true;
 	}
 
@@ -39,41 +34,62 @@ class Plugin_nginx_http_push_Helper_http_push extends Helper {
 	*
 	* @return results
 	*/
-	private function http($url, $method, $postfields = NULL) {
+	private function http($urls, $method, $channel, $postfields = NULL) {
 		$this->http_info = array();
-		$ci = curl_init();
-		/* Curl settings */
-		curl_setopt($ci, CURLOPT_USERAGENT, $this->useragent);
-		curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, $this->connecttimeout);
-		curl_setopt($ci, CURLOPT_TIMEOUT, $this->timeout);
-		curl_setopt($ci, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($ci, CURLOPT_HTTPHEADER, array('Expect:'));
-		curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, $this->ssl_verifypeer);
-		curl_setopt($ci, CURLOPT_HEADERFUNCTION, array($this, 'getHeader'));
-		curl_setopt($ci, CURLOPT_HEADER, FALSE);
+		$cm = curl_multi_init();
 
-		switch ($method) {
-			case 'GET':
-				curl_setopt($ci, CURLOPT_GET, TRUE);
-				break;
-			case 'POST':
-				curl_setopt($ci, CURLOPT_POST, TRUE);
-				if (!empty($postfields)) {
-					curl_setopt($ci, CURLOPT_POSTFIELDS, ($this->format == 'json' ? json_encode($postfields) : $postfields));
-				}
-				break;
-			case 'DELETE':
-				curl_setopt($ci, CURLOPT_CUSTOMREQUEST, 'DELETE');
-				if (!empty($postfields)) {
-					$url = "{$url}?{$postfields}";
-				}
-		}
-		curl_setopt($ci, CURLOPT_URL, $url);
-		$response = curl_exec($ci);
-		$this->http_code = curl_getinfo($ci, CURLINFO_HTTP_CODE);
-		$this->http_info = array_merge($this->http_info, curl_getinfo($ci));
-		curl_close ($ci);
-		return $response;
+        $curlHandles = array();
+        $responses = array();
+
+        foreach($urls as $id => $url) {
+            $c = curl_init();
+            /* Curl settings */
+            curl_setopt($c, CURLOPT_USERAGENT, $this->useragent);
+            curl_setopt($c, CURLOPT_CONNECTTIMEOUT, $this->connecttimeout);
+            curl_setopt($c, CURLOPT_TIMEOUT, $this->timeout);
+            curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($c, CURLOPT_HTTPHEADER, array('Expect:'));
+            curl_setopt($c, CURLOPT_SSL_VERIFYPEER, $this->ssl_verifypeer);
+            curl_setopt($c, CURLOPT_HEADERFUNCTION, array($this, 'getHeader'));
+            curl_setopt($c, CURLOPT_HEADER, FALSE);
+
+            switch ($method) {
+                case 'GET':
+                    curl_setopt($c, CURLOPT_GET, TRUE);
+                    break;
+                case 'POST':
+                    curl_setopt($c, CURLOPT_POST, TRUE);
+                    if (!empty($postfields)) {
+                        curl_setopt($c, CURLOPT_POSTFIELDS, ($this->format == 'json' ? json_encode($postfields) : $postfields));
+                    }
+                    break;
+                case 'DELETE':
+                    curl_setopt($c, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                    if (!empty($postfields)) {
+                        $url = "{$url}?{$postfields}";
+                    }
+            }
+
+            curl_setopt($c, CURLOPT_URL, $url.'?'.$channel);
+
+            $curlHandles[$id] = $c;
+            curl_multi_add_handle($cm, $curlHandles[$id]);
+        }
+        
+        $running = null;
+        do {
+            curl_multi_exec($cm, $running);
+        } while($running > 0);
+
+        $result = FALSE;
+        foreach($curlHandles as $id => $handle) {
+            $responses[$id] = curl_multi_getcontent($handle);
+            $result = (bool)curl_multi_remove_handle($cm, $handle) || $result;
+        }
+
+        curl_multi_close($cm);
+
+		return $result;
 	}
 
 	/**
